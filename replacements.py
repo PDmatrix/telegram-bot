0270d93231ecb263f779594370c22d78984e7e14
PDmatrix <dimasebakovgoo@gmail.com>
2017-10-07 22:55:42 +0300

    v0.5 Improved replacements

--- replacements.py
+++ replacements.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
 def findChange(group, day):
@@ -8,24 +9,37 @@ def findChange(group, day):
     lines = soup.find_all("tr")
     if len(lines) == 0:
         return "Расписание не готово."
+    if len(lines[0].p.text) != 49:
+        return "Что-то не так. Проверьте замены вручную."
     for i in range(5,len(lines) - 1):
         strs = lines[i].find_all("p")
-        ans = ""
-        rem = ""
-        fin = ""
+        ans = ""#Временный держатель
+        rem = ""#Переменная для запоминания пары
+        fin = []
+        ans2 = ""#Финальный ответ
         if strs[0].text.lower() == group.lower():
             for k in range(i + 1,len(lines) - 1):
                 for j in range(1,len(strs)):
                     if strs[j].text == "1 п/г":
                         rem = strs[j + 1].text
-                    ans += strs[j].text + " "
+                    ans += strs[j].text + ";"
                     if strs[j].text == "2 п/г":
-                        ans += rem + " "
+                        ans += rem + ";"
                 strs = lines[k].find_all("p")
                 if len(strs[0].text) != 1:
                     break
-                ans += "\n"
-            return ans
-    if ans == "":
+                
+                #Удаление всех управляющийх символов
+                mpa = dict.fromkeys(range(32))
+                fin = ans.translate(mpa).split(";")
+                
+                ans = ""
+                if fin[0] == "1 п/г" or fin[0] == "2 п/г":
+                    ans2 += fin[1] + " пара: " + fin[0] +" "+ fin[2] +". " + fin[3] + " " + fin[4] + "\n"
+                else:
+                    ans2 += fin[1] + " пара: " + fin[2] + ". " + fin[3] + " " + fin[4]
+            #Возвращение даты и замен
+            return lines[1].p.text.translate(mpa) + "\n" + ans2
+    if ans2 == "":
         return "Нет замен."
 
