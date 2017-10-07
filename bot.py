365c51a9a4a3812ac76450ec5d0151dec6fa074f
PDmatrix <dimasebakov@gmail.com>
2017-10-07 19:39:36 +0300

    v.0.4 Heroku WebHook

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import logging, answers, replacements, schedule
+import logging, answers, replacements, schedule, os
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -49,8 +49,9 @@ def error(bot, update, error):
 
 def main():
     # Create the EventHandler and pass it your bot's token.
-    updater = Updater("462202131:AAETYcmO8qi2m1SaQzs-zzqC_ycRHOqXG14")
-
+    TOKEN = "462202131:AAETYcmO8qi2m1SaQzs-zzqC_ycRHOqXG14"
+    PORT = int(os.environ.get('PORT', '5000'))
+    updater = Updater(TOKEN)
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
@@ -67,8 +68,11 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_polling()
 
+    #updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0", port = PORT, url_path = TOKEN)
+    APPNAME = "telegrambotchemk"
+    updater.bot.set_webhook("https://{}.herokuapp.com/".format(APPNAME) + TOKEN)
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
