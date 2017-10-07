2bccfd04dfcc0e1bf18f89269c84b57c274e335a
PDmatrix <dimasebakov@gmail.com>
2017-10-07 15:33:40 +0300

    minor changes

--- schedule.py
+++ schedule.py
@@ -61,7 +61,7 @@ def getSchedule(day):
     questions = f.readlines()
     answer = ""
     for i in range(0,len(questions)):
-        #print(getNum(questions[i]))
+        print(len(questions[i]))
         if getNum(questions[i]) == week:
             for j in range(i, len(questions)):
                 if len(questions[j]) == 2:
