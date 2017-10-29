1dbbf121398c836012e501c639dcad20529b9d27
PDmatrix <dimasebakovgoo@gmail.com>
2017-10-29 18:11:47 +0300

    v0.7 Change to a new table style

--- replacements.py
+++ replacements.py
@@ -36,12 +36,15 @@ def findChange(group = "пр1-15", day = "завтра"):
     cont = site.content
     soup = BeautifulSoup(cont, 'html.parser')
     lines = soup.find_all("tr")
+    l = 0
     if len(lines) == 0:
         return "Расписание не готово."
     if len(lines[0].p.text) != 49:
         return "Что-то не так. Проверьте замены вручную."
     for i in range(5,len(lines)):
         strs = lines[i].find_all("p")
+        if strs == []:
+            continue
         ans = ""#Временный держатель
         rem = ""#Переменная для запоминания пары
         fin = []
@@ -55,21 +58,38 @@ def findChange(group = "пр1-15", day = "завтра"):
                     if strs[j].text == "2 п/г":
                         ans += rem + ";"
                 strs = lines[k].find_all("p")
-                if len(strs[0].text) != 1:
+                strs.insert(0,'')
+                if strs == []:
                     break
-                
+                #Проверка на концовку группы
+                if ans[0] == u'\xa0' and ans[2] == u'\xa0' and ans[4] == u'\xa0': 
+                    break
+                #Удаление первого символа
+                if ans[0] == u'\xa0':
+                    ans = ans[2:]
+
+                #if l == 1:
+                    #return ans
+                #l += 1
+
                 #Удаление всех управляющийх символов
                 mpa = dict.fromkeys(range(32))
                 fin = ans.translate(mpa).split(";")
-        
                 ans = ""
-                if fin[0] == "1 п/г" or fin[0] == "2 п/г":
+                if len(fin) == 3:
+                    ans2 += fin[0] + " пара: " + fin[1] + "\n"
+                    break
+                elif len(fin) == 4:
+                    ans2 += fin[1] + " пара: " + fin[0] + " " + fin[2] + "\n"
+                    break
+                elif fin[0] == "1 п/г" or fin[0] == "2 п/г":
                     ans2 += fin[1] + " пара: " + fin[0] +" "+ fin[2] +". " + fin[3] + " " + fin[4] + "\n"
                 else:
-                    ans2 += fin[1] + " пара: " + fin[2] + ". " + fin[3] + " " + fin[4] + "\n"
+                    ans2 += fin[0] + " пара: " + fin[1] + ". " + fin[2] + " " + fin[3] + "\n"
+                    
             #Возвращение даты и замен
             return lines[1].p.text.translate(mpa) + "\n" + ans2
     if ans2 == "":
         return "Нет замен."
 #print(getStar("today"))
-#print(findChange("кс5-16","tomorrow"))
+#print(findChange("пр1-15","сегодня"))
