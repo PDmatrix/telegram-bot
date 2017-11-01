ac9bd0b3ea55e195690d80151fbdb3b18b6fa672
LiberBear <mihailfs@yandex.ru>
2017-11-01 21:33:50 +0300

    Исправлено ситуация с переменами
    Время перемены учитывалось как конец всех пар

--- zvonki.py
+++ zvonki.py
@@ -3,9 +3,9 @@ from datetime import datetime, timedelta
 zvonki = [['8:15', '9:55'], ['10:25', '11:55'], ['12:05', '13:35'], ['14:00', '15:35'], ['15:45', '17:20'], ['17:30', '19:05']]
 zvonki_vtornik = [['8:15', '9:55'], ['10:25', '11:55'], ['12:05', '12:50'], ['13:00', '14:30'], ['14:50', '16:30'], ['16:40', '18:15'], ['18:25', '20:00']]
 
-#За такой костыль надо бить по рукам
 def current_time():
 	return datetime.strptime(str(datetime.now().hour)+str(datetime.now().minute), "%H%M")
+	#return datetime.strptime('10:00', '%H:%M')
 
 #Возврат расписания с проверкой на вторник
 def raspisanie():
@@ -14,15 +14,23 @@ def raspisanie():
 	else:
 		return zvonki
 	
-#Возвращает номер текущей пары если пар нет то 0
+#Возвращает номер текущей пары если пар нет то 0, если перемена то -1
 def get_para_num():
 	para_num = 0
 	for idx, para in enumerate(raspisanie()):
 		para_st = datetime.strptime(para[0], "%H:%M")
 		para_end = datetime.strptime(para[1], "%H:%M")
+		try:
+			para_next = datetime.strptime(raspisanie()[idx+1][0], "%H:%M")
+		except:
+			pass
+		#Проверка на пару
 		if para_st < current_time() and current_time() < para_end:
 			#print('Сейчас идет {} пара'.format(idx+1))
 			para_num = idx+1
+		#Проверка на перемену
+		if current_time()  > para_end and current_time() < para_next:
+			para_num = -1
 	return para_num
 
 #Принимает номер пары, возвращает оставшиеся до конца время
@@ -30,12 +38,16 @@ def when_para_ends(para_num):
 	para_end = datetime.strptime(raspisanie()[para_num-1][1], "%H:%M")
 	remaining = para_end - current_time()
 	return remaining
+
 #Принимает номер пары возвращает время до ее начала
+#Пока не готово
+'''
 def when_para_start(para_num):
 	para_st = datetime.strptime(raspisanie()[para_num-1][0], "%H:%M")
-	remaining = para_st  - current_time()
+	remaining = para_st-current_time()
 	return remaining
-#print(datetime.strptime(when_para_start(1), "%H:%M"))
+'''
 #print('Время: {}'.format(datetime.strftime(current_time(), "%H:%M")))
+#
 #print('Осталось {} пары'.format(get_para_num()))
 #print('До конца пары {} ч.'.format(when_para_ends(get_para_num())))
