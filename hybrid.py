8db09a9333995ebe3cd69062f98447d5f429e3a9
PDmatrix <dimasebakovgoo@gmail.com>
2018-01-25 23:12:20 +0300

    v1.5 New improved version

--- hybrid.py
+++ hybrid.py
@@ -1,37 +1,55 @@
-import replacements, schedule
- 
-def getHybrid(group = "пр1-15", day = "завтра"):
-    zamen = replacements.findChange(group,day).replace(u'\xa0','').split('\n')
-    rsps = schedule.getSchedule(group, day).replace(u'\xa0','').replace(u'\ufeff','').replace(u'\r','').split('\n')
-    if zamen[0] == "Расписание не готово.":
-        return zamen[0]
-    elif zamen[0] == "Что-то не так. Проверьте замены вручную.":
+import replacements
+import schedule
+
+
+def getHybrid(group="пр1-15", day="завтра"):
+    zamen = replacements.findChange(group, day).replace(u'\xa0',
+                                                        '').split('\n')
+    rsps = schedule.getSchedule(group, day).replace(u'\xa0', '').replace(
+        u'\ufeff', '').replace(u'\r', '').split('\n')
+    newRsps = []
+    newRsps.append(rsps[0])
+    for i in range(1, len(rsps) - 1):
+        if rsps[i].find('*') == -1:
+            newRsps.append(rsps[i])
+            continue
+        else:
+            if rsps[i].find('**') != -1:
+                if replacements.getStar(day) == 2:
+                    newRsps.append(rsps[i])
+            else:
+                if replacements.getStar(day) == 1:
+                    newRsps.append(rsps[i])
+    rsps = newRsps
+    if zamen[0] == "Расписание не готово." or \
+        zamen[0] == "Что-то не так. Проверьте замены вручную." or \
+            zamen[0] == "Сервер недоступен.":
         return zamen[0]
     elif zamen[0] == "Нет замен.":
-        return schedule.getSchedule(group, day)
-    elif zamen[0] == "Сервер недоступен.":
-        return zamen[0]
-    #if rsps[0] == "Пятница":
-    #    if replacements.getStar("tomorrow") == 1:
-    #        del rsps[3]
-    #    else:
-    #        del rsps[2]
-    last = ""
-    zm = 0
-    fr = True
-    for i in range(0, len(rsps) - 1):
-        for j in range(1 + zm, len(zamen)):
-            if zamen[j][0:6] == rsps[i][0:6]:
-                if fr == True:
-                    rsps[i] = zamen[j]
-                else:
-                    rsps[i] +="\n" + zamen[j]
-                zm += 1
-                fr = False
-        rsps[i] += "\n"
-        fr = True
-    if len(zamen)-zm != 2:
-        for i in range(1 + zm, len(zamen) - 1):
-            rsps.append(zamen[i] + "\n")
-    ans = ''.join(rsps).rstrip('\n')
+        return '\n'.join(rsps)
+    newZamen = []
+    newZamen.append(zamen[0])
+    for i in range(1, len(zamen) - 1):
+        zamPar = zamen[i][0:zamen[i].find("пара")].replace(' ', '').split(',')
+        if len(zamPar) != 1:
+            for j in range(0, len(zamPar)):
+                newZamen.append(zamPar[j] +
+                                ' ' +
+                                zamen[i][zamen[i].find("пара"):len(zamen[i])]
+                                )
+    zamen = newZamen if len(newZamen) > 1 else zamen
+    del newZamen
+    for i in range(1, len(zamen)):
+        for j in range(1, len(rsps)):
+            if zamen[i][0:6] == rsps[j][0:6]:
+                del rsps[j]
+                break
+    for i in range(1, len(zamen)):
+        rsps.append(zamen[i])
+    rsps = [line for line in rsps if line != '']
+    tempDay = rsps[0]
+    rsps = rsps[1:len(rsps)]
+    rsps.sort(key=lambda x: int(x[0]))
+    rsps.insert(0, tempDay)
+    ans = '\n'.join(rsps[0:len(rsps)])
     return ans
